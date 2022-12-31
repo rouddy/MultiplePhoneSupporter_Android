10a25760973ac6776f6f4847fb44bdc7af96c395
Jaehong Yoo <rouddy@naver.com>
2022-12-31 10:59:09 +0900

    notification 가져오기 추가

--- app/src/main/java/com/rouddy/twophonesupporter/MainActivity.kt
+++ app/src/main/java/com/rouddy/twophonesupporter/MainActivity.kt
@@ -1,11 +1,33 @@
 package com.rouddy.twophonesupporter
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationManagerCompat
+import com.rouddy.twophonesupporter.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.checkButton.setOnClickListener {
+            if (!checkNotificationPermissionGrantred()) {
+                val intent = Intent("android.settings.ACTION_NOTIFICATION_LISTENER_SETTINGS")
+                startActivity(intent)
+            }
+        }
+    }
+
+    private fun checkNotificationPermissionGrantred(): Boolean {
+        val sets = NotificationManagerCompat.getEnabledListenerPackages(this)
+        Log.e("!!!", "enabled:${sets.joinToString(",")}")
+        return sets.contains(packageName)
     }
 }
--- app/src/main/java/com/rouddy/twophonesupporter/MyNotificationListenerService.kt
+++ app/src/main/java/com/rouddy/twophonesupporter/MyNotificationListenerService.kt
@@ -1,6 +1,38 @@
 package com.rouddy.twophonesupporter
 
 import android.service.notification.NotificationListenerService
+import android.service.notification.StatusBarNotification
+import android.util.Log
 
 class MyNotificationListenerService : NotificationListenerService() {
+    override fun onCreate() {
+        Log.e("!!!", "MyNotificationListenerService::onCreate")
+        super.onCreate()
+    }
+
+    override fun onDestroy() {
+        Log.e("!!!", "MyNotificationListenerService::onDestroy")
+        super.onDestroy()
+    }
+
+    override fun onListenerConnected() {
+        super.onListenerConnected()
+        Log.e("!!!", "onListenerConnected")
+    }
+
+    override fun onListenerDisconnected() {
+        super.onListenerDisconnected()
+        Log.e("!!!", "onListenerDisconnected")
+    }
+
+    override fun onNotificationPosted(sbn: StatusBarNotification?) {
+        super.onNotificationPosted(sbn)
+        Log.e("!!!", "000 onNotificationPosted:$sbn")
+    }
+
+    override fun onNotificationRemoved(sbn: StatusBarNotification?) {
+        super.onNotificationRemoved(sbn)
+        Log.e("!!!", "222 onNotificationRemoved:$sbn")
+    }
+
 }
